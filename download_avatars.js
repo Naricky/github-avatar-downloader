ff6f086809109fb19d479fdaa47e1d97ee666800
Ricky Lee <Na_ricky@hotmail.com>
2017-08-01 19:51:54 +0000

    final commit

--- download_avatars.js
+++ download_avatars.js
@@ -1,54 +1,56 @@
-var a = process.argv.slice(2);
-
+var para = process.argv.slice(2);
 var request = require('request');
 var fs = require('fs');
 
 var GITHUB_USER = "Naricky"
-var GITHUB_TOKEN = "70617a6478537100de86ccf1e8822460df199b43"
-
+var GITHUB_TOKEN = "a4a1416bd48509e756e76ebfea4a9a968b46bb1f"
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-
+  console.log(requestURL);
 
   request.get({
-            url: requestURL,
-            headers: {
-              'User-Agent': 'request'
-            }
-          })
-          .on('error', function (err) {
-           // throw err
-           console.log("Errors:", err);
+    url: requestURL,
+    headers: {
+      'User-Agent': 'request'
+    }
+    })
+  .on('error', function (err) {
 
-          })
-         .on('response', function (response) {
-            console.log(response.statusCode);
-            // console.log("Result:", response);
-            var body = '';
-            response.on('data', function (chunk) {
-              body += chunk;
-            });
-            response.on('end', function () {
-              var result = JSON.parse(body);
-              console.log('BODY: ', result);
-              cb(null, result)
-            });
+    console.log("Errors:", err);
 
-          })
+  })
+  .on('response', function (response) {
+    console.log(response.statusCode);
+
+    var body = '';
+    response.on('data', function (chunk) {
+      body += chunk;
+    });
+      response.on('end', function () {
+        var result = JSON.parse(body);
+        console.log('BODY: ', result);
+        cb(null, result)
+      });
+
+})
 
 
 }
 
-getRepoContributors(a[0], a[1], function(err, result) {
+getRepoContributors("jquery", "jquery", function(err, result) {
 
- for (var i = 0; i < result.length; i++) {
-   downloadImageByURL(result[i].avatar_url, "avatars/"+result[i].login);
-
+  if (!para[0] || !para[1] ){
+   console.log ("ERROR - NO INPUT")
+}
+  else {
+    for (var i = 0; i < result.length; i++) {
+      downloadImageByURL(result[i].avatar_url, "avatars/"+result[i].login);
  }
+}
 });
 
 function downloadImageByURL(url, filepath){
@@ -61,3 +63,5 @@ function downloadImageByURL(url, filepath){
    })
    .pipe(fs.createWriteStream(filepath));
 }
+
+
